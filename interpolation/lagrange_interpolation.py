e97f795bef5b096d2344dec5e3b3653e6aa50f47
Ramin Tawakuli <rtawakuli@gmail.com>
2012-11-03 16:49:10 -0700

    Revised coding style in lagrange_interpolation file

--- interpolation/lagrange_interpolation.py
+++ interpolation/lagrange_interpolation.py
@@ -1,11 +1,11 @@
 from numpy import poly1d
 
 """
-This Modules implements the Lagrange Interpolating polynomial.
+This file implements the Lagrange Interpolating polynomial.
 """
 
-"create a new list of x values without the" 
-"x value located at position [index] in the array"
+# create a new list of x values without the
+# x value located at position [index] in the array
 def xvals_not_i(x_values,index):
     new_x_values = []
     
@@ -16,30 +16,30 @@ def xvals_not_i(x_values,index):
         
     return new_x_values
 
-"Create a polynomial with roots that are all Xs except X_index"
-def BasisNumerator(x_values, index):
+# Create a polynomial with roots that are all Xs except X_index
+def basis_numerator(x_values, index):
     new_x_list = xvals_not_i(x_values,index)
-    Basis_Numerator = poly1d(new_x_list,True)
-    return Basis_Numerator
+    basis_poly_numerator = poly1d(new_x_list,True)
+    return basis_poly_numerator
 
-"Create a Lagrange Basis polynomial of degree n-1 for x_i for lagrange interpolation"
-def LangrangeBasisPoly(x_valus, index):
-    Numerator = BasisNumerator(x_valus, index)
+# Create a Lagrange Basis polynomial of degree n-1 for x_i for lagrange interpolation
+def langrange_basis_poly(x_valus, index):
+    Numerator = basis_numerator(x_valus, index)
     Denominator = Numerator(x_valus[index])
-    BasisPoly = Numerator / Denominator
-    return BasisPoly
+    basis_poly = Numerator / Denominator
+    return basis_poly
 
-"Create a lagrange interpolating polynomial"
-def lagrangePoly(X_values,Y_values):
-    LagPoly = 0 #initialize
+# Create a lagrange interpolating polynomial
+def lagrange_poly(X_values,Y_values):
+    lag_poly = 0 #initialize
     
     #build interpolating polynomial  
     for i in range(len(X_values)):
-        LagPoly = LagPoly + Y_values[i] * LangrangeBasisPoly(X_values, i)
+        lag_poly += Y_values[i] * langrange_basis_poly(X_values, i)
          
-    return LagPoly
+    return lag_poly
 
-"Create a polynomial whose roots are the values in the array x_values"
+# Create a polynomial whose roots are the values in the array x_values
 def pi_ofX(x_values):
     pi_poly = 1
     for x in x_values:
